85c808aefa5fde2d6a444287e9c4cfab135ffc65
Nina Morozova <pianina@MacBook-Pro-Nina.local>
2018-08-21 11:25:52 +0300

    first commit

---
+++ Studio soul/src/js/index.js
@@ -0,0 +1,28 @@
+'use strict';
+/**
+ * App entry point.
+ *
+ * @module App
+ */
+
+/** Import initialized-by-default modules/libs */
+import './components/Common';
+import './components/PublicAPI';
+
+/** Import page controllers */
+import Home from './pages/Home';
+
+import { currentPage } from './modules/dev/_helpers';
+
+/**
+ * Run appropriate scripts for each page.
+ **/
+switch (currentPage) {
+  /** Home page */
+  case 'home': new Home; break;
+
+  /** No page found */
+  default: console.warn('Undefined page');
+}
+console.log('working');
+document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>');
